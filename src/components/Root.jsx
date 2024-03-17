aa679c20bac44a9be491d8028df21773fe553ab4
Daphne vdh <d-frontenddeveloper@outlook.com>
2024-03-17 20:15:24 +0100

    cleaning not needed context

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { Box } from "@chakra-ui/react";
-import { NotificationProvider } from "../contexts/NotificationContext";
-import { Notifications } from "./notification/Notifications";
 import { SiteContextProvider } from "../contexts/SiteContext";
 
 export const Root = () => {
@@ -11,10 +9,7 @@ export const Root = () => {
     <Box>
       <Navigation />
       <SiteContextProvider>
-        <NotificationProvider>
-          <Notifications />
-          <Outlet />
-        </NotificationProvider>
+        <Outlet />
       </SiteContextProvider>
     </Box>
   );
