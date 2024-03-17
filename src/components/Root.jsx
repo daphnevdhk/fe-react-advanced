3746e0c68541382427e1ceba57dbd6049b0c9e2d
Daphne vdh <d-frontenddeveloper@outlook.com>
2024-03-17 20:24:16 +0100

    cleaning

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Navigation } from "./Navigation";
-import { Box } from "@chakra-ui/react";
 import { SiteContextProvider } from "../contexts/SiteContext";
 
 export const Root = () => {
   return (
-    <Box>
-      <Navigation />
-      <SiteContextProvider>
-        <Outlet />
-      </SiteContextProvider>
-    </Box>
+    <SiteContextProvider>
+      <Outlet />
+    </SiteContextProvider>
   );
 };
