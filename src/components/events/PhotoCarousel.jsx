14ed53bce992101856078854797f96cf17ee06cb
Daphne vdh <d-frontenddeveloper@outlook.com>
2024-03-18 22:08:05 +0100

    added more picture to carousel

--- src/components/events/PhotoCarousel.jsx
+++ src/components/events/PhotoCarousel.jsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 export const PhotoCarousel = () => {
   const images = [
     "https://ymcasouthflorida.org/wp-content/uploads/2019/12/AA2I8910-scaled.jpg",
+    "https://www.ctvnews.ca/polopoly_fs/1.5118552.1600965663!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg",
     "https://th.bing.com/th/id/R.ce44791bbda5945db3f1cecd41d4ef37?rik=O1lU%2bFv4JWGG5Q&pid=ImgRaw&r=0",
+    "https://s3.eu-west-2.amazonaws.com/ws-activestirling/production/Badminton/_1200x630_crop_center-center_none/Badminton-3.jpg",
   ]; // replace with your image paths
   const [index, setIndex] = useState(0);
 
