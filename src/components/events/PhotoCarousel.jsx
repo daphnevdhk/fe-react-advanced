f58e1853cbe17fb6cf73b166a2f5e7e4f469a319
Daphne vdh <d-frontenddeveloper@outlook.com>
2024-03-23 16:54:20 +0100

    improve PhotoCarousel

--- src/components/events/PhotoCarousel.jsx
+++ src/components/events/PhotoCarousel.jsx
@@ -3,10 +3,38 @@ import { useEffect, useState } from "react";
 
 export const PhotoCarousel = () => {
   const images = [
-    "https://ymcasouthflorida.org/wp-content/uploads/2019/12/AA2I8910-scaled.jpg",
-    "https://www.ctvnews.ca/polopoly_fs/1.5118552.1600965663!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg",
-    "https://th.bing.com/th/id/R.ce44791bbda5945db3f1cecd41d4ef37?rik=O1lU%2bFv4JWGG5Q&pid=ImgRaw&r=0",
-    "https://s3.eu-west-2.amazonaws.com/ws-activestirling/production/Badminton/_1200x630_crop_center-center_none/Badminton-3.jpg",
+    <Image
+      key="1"
+      fallback={<Skeleton h={"100%"} w={"100%"} />}
+      fit="cover"
+      h={"100%"}
+      w={"100%"}
+      src="https://ymcasouthflorida.org/wp-content/uploads/2019/12/AA2I8910-scaled.jpg"
+    />,
+    <Image
+      key="2"
+      fallback={<Skeleton h={"100%"} w={"100%"} />}
+      fit="cover"
+      h={"100%"}
+      w={"100%"}
+      src="https://www.ctvnews.ca/polopoly_fs/1.5118552.1600965663!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg"
+    />,
+    <Image
+      key="3"
+      fallback={<Skeleton h={"100%"} w={"100%"} />}
+      fit="cover"
+      h={"100%"}
+      w={"100%"}
+      src="https://th.bing.com/th/id/R.ce44791bbda5945db3f1cecd41d4ef37?rik=O1lU%2bFv4JWGG5Q&pid=ImgRaw&r=0"
+    />,
+    <Image
+      key="4"
+      fallback={<Skeleton h={"100%"} w={"100%"} />}
+      fit="cover"
+      h={"100%"}
+      w={"100%"}
+      src="https://s3.eu-west-2.amazonaws.com/ws-activestirling/production/Badminton/_1200x630_crop_center-center_none/Badminton-3.jpg"
+    />,
   ]; // replace with your image paths
   const [index, setIndex] = useState(0);
 
@@ -20,13 +48,7 @@ export const PhotoCarousel = () => {
 
   return (
     <Box height={"xs"} width="100%">
-      <Image
-        fallback={<Skeleton h={"100%"} w={"100%"} />}
-        src={images[index]}
-        fit="cover"
-        h={"100%"}
-        w={"100%"}
-      />
+      {images[index]}
     </Box>
   );
 };
