ec67ffd2fd9d1d63cf1aeb2d4c3e6faff4264580
Daphne vdh <d-frontenddeveloper@outlook.com>
2024-03-24 13:01:33 +0100

    Removed not used hook

--- src/hooks/use-notification-context.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import NotificationContext from "../contexts/NotificationContext";
-
-export const useNotificationContext = () => {
-  return useContext(NotificationContext);
-};
